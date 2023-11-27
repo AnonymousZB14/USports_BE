9dffa5c1e988ab14a76b391b523904f854e40714
hsryuDesktop <happyhsryu@gmail.com>
2023-11-27 14:27:31 +0900

    initial commit

---
+++ src/main/java/com/anonymous/usports/UsportsApplication.java
@@ -0,0 +1,13 @@
+package com.anonymous.usports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsportsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anonymous/usports/UsportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonymous.usports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
