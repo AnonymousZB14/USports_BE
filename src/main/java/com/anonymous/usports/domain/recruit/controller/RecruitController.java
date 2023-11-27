9edf333171f5f2408234240fe216d07f91435dc4
HyunSik Ryu <hsryu123@naver.com>
2023-11-28 07:50:24 +0900

    Refactor: AddRecruit -> RecruitRegister (명사+동사 형태)로 변수명 변경, Add 대신 Register 단어를 사용하도록 메서드명 변경

--- src/main/java/com/anonymous/usports/domain/recruit/controller/RecruitController.java
+++ src/main/java/com/anonymous/usports/domain/recruit/controller/RecruitController.java
@@ -1,12 +1,13 @@
 package com.anonymous.usports.domain.recruit.controller;
 
-import com.anonymous.usports.domain.member.entity.MemberEntity;
-import com.anonymous.usports.domain.recruit.dto.AddRecruit;
+import com.anonymous.usports.domain.recruit.dto.RecruitRegister;
 import com.anonymous.usports.domain.recruit.dto.RecruitDto;
 import com.anonymous.usports.domain.recruit.service.RecruitService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,14 +19,14 @@ public class RecruitController {
 
   private final RecruitService recruitService;
 
-
+  @ApiOperation("운동 모집 게시글 등록하기")
   @PostMapping("/recruit")
-  public ResponseEntity<?> addRecruit(@RequestBody AddRecruit.Request request){
+  public ResponseEntity<?> registerRecruit(@RequestBody RecruitRegister.Request request){
     //수정
     Long memberId = 0L;
     RecruitDto result = recruitService.addRecruit(request, memberId);
 
-    return ResponseEntity.ok(AddRecruit.Response.fromDto(result));
+    return ResponseEntity.ok(RecruitRegister.Response.fromDto(result));
   }
 
 
