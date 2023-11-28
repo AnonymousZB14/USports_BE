b9d4b2f50faad8c2546602fad0d3be2290e1fc0a
HyunSik Ryu <hsryu123@naver.com>
2023-11-28 21:03:49 +0900

    Feat : Participant 리스트 조회(page) 구현
    - 회원에게 page는 1부터 시작, Page<>는 0부터 시작

--- src/main/java/com/anonymous/usports/domain/participant/controller/ParticipantController.java
+++ src/main/java/com/anonymous/usports/domain/participant/controller/ParticipantController.java
@@ -1,16 +1,19 @@
 package com.anonymous.usports.domain.participant.controller;
 
+import com.anonymous.usports.domain.participant.dto.ParticipantDto;
+import com.anonymous.usports.domain.participant.dto.ParticipantListDto;
 import com.anonymous.usports.domain.participant.dto.ParticipantManage;
 import com.anonymous.usports.domain.participant.dto.ParticipantResponse;
-import com.anonymous.usports.domain.participant.dto.ParticipantDto;
 import com.anonymous.usports.domain.participant.service.ParticipantService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -19,6 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ParticipantController {
 
   private final ParticipantService participantService;
+
+  @GetMapping("/recruit/{recruitId}/applicants")
+  public ResponseEntity<?> getApplicants(@PathVariable Long recruitId,
+      @RequestParam(name = "page", defaultValue = "1") int page) {
+    Long memberId = 0L;//FIXME : @AuthenticationPrincipal 에서 memberId 불러오기
+    ParticipantListDto result = participantService.getParticipants(recruitId, page, memberId);
+    return ResponseEntity.ok(result);
+  }
 
   @ApiOperation(value = "운동 모집 게시글에 참여 신청 넣기", notes = "Participant entity 생성")
   @PostMapping("/recruit/{recruitId}/join")
@@ -38,7 +49,8 @@ public class ParticipantController {
 
     Long memberId = 0L;//FIXME : @AuthenticationPrincipal 에서 memberId 불러오기
 
-    ParticipantManage.Response result = participantService.manageJoinRecruit(request, recruitId, memberId);
+    ParticipantManage.Response result = participantService.manageJoinRecruit(request, recruitId,
+        memberId);
 
     return ResponseEntity.ok(result);
   }
