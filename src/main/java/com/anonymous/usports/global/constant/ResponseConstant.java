359fe2d36ef72225dac4b8f9ab65c0644ef96af3
HyunSik Ryu <hsryu123@naver.com>
2023-11-28 12:18:52 +0900

    Chore: 주석, 오타 수정

--- src/main/java/com/anonymous/usports/global/constant/ResponseConstant.java
+++ src/main/java/com/anonymous/usports/global/constant/ResponseConstant.java
@@ -1,6 +1,8 @@
 package com.anonymous.usports.global.constant;
 
 public class ResponseConstant {
+
+  // 운동 모집 관련
   public static final String REGISTER_RECRUIT = "모임이 저장되었습니다.";
   public static final String UPDATE_RECRUIT = "모임이 수정 되었습니다.";
   public static final String DELETE_RECRUIT = "모임이 삭제 되었습니다.";
