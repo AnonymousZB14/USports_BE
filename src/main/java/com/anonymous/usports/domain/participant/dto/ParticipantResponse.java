edb16c4b9032b2f9c94cb90b31b82255aa0e1b5b
HyunSik Ryu <hsryu123@naver.com>
2023-11-29 17:43:25 +0900

    test : Participant, Recruit 관련 현재까지 api 테스트 완료

--- src/main/java/com/anonymous/usports/domain/participant/dto/ParticipantResponse.java
+++ src/main/java/com/anonymous/usports/domain/participant/dto/ParticipantResponse.java
@@ -3,6 +3,17 @@ package com.anonymous.usports.domain.participant.dto;
 
 import com.anonymous.usports.global.constant.ResponseConstant;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ParticipantResponse {
   private Long recruitId;
   private Long memberId;
