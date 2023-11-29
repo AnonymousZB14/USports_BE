e74c4048dfda2c0811ef749bc24233de0c5ace44
HyunSik Ryu <hsryu123@naver.com>
2023-11-29 13:50:01 +0900

    Fix: @Getter가 없어서 Recruit 삭제 api 호출 시 발생하던 406 에러 수정

--- src/main/java/com/anonymous/usports/domain/recruit/dto/RecruitDeleteResponse.java
+++ src/main/java/com/anonymous/usports/domain/recruit/dto/RecruitDeleteResponse.java
@@ -1,13 +1,23 @@
 package com.anonymous.usports.domain.recruit.dto;
 
 import com.anonymous.usports.global.constant.ResponseConstant;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RecruitDeleteResponse {
 
   private Long recruitId;
   private String message;
 
-  public RecruitDeleteResponse(RecruitDto recruitDto){
+  public RecruitDeleteResponse(RecruitDto recruitDto) {
     this.recruitId = recruitDto.getRecruitId();
     this.message = ResponseConstant.DELETE_RECRUIT;
   }
