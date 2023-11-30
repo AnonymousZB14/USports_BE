b7dae1417aff403eea68eece2b99b3269c527216
HyunSik Ryu <hsryu123@naver.com>
2023-12-01 00:46:04 +0900

    Test: contextLoad test 비활성화

--- src/test/java/com/anonymous/usports/UsportsApplicationTests.java
+++ src/test/java/com/anonymous/usports/UsportsApplicationTests.java
@@ -3,10 +3,10 @@ package com.anonymous.usports;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+//@SpringBootTest
 class UsportsApplicationTests {
 
-
+    @Test
     void contextLoads() {
     }
 
