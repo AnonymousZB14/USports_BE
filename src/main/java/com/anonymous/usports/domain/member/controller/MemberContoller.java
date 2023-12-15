f824598445840e0ee671a681426c2c92d1bcd73c
HyunSik Ryu <hsryu123@naver.com>
2023-12-15 14:23:18 +0900

    Refactor : 로그인 시 반환 DTO에 Member 정보 추가

--- src/main/java/com/anonymous/usports/domain/member/controller/MemberContoller.java
+++ src/main/java/com/anonymous/usports/domain/member/controller/MemberContoller.java
@@ -52,6 +52,7 @@ public class MemberContoller {
         notificationService.checkUnreadNotificationAndSetSession(memberDto.getMemberId(), httpServletRequest);
 
         return ResponseEntity.ok(MemberLogin.Response.builder()
+                .member(memberDto)
                         .tokenDto(tokenProvider.saveTokenInRedis(memberDto.getEmail()))
                 .build());
     }
