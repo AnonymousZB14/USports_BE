9a2f0464a03ba46efd818bfb4e763f01b32af882
HyunSik Ryu <hsryu123@naver.com>
2023-12-16 15:53:52 +0900

    Refactor : ProfileController 메서드 2개 -> 3개로 변경
    - 멤버 정보
    - 기록글 정보
    - 모집글 정보

--- src/main/java/com/anonymous/usports/domain/profile/controller/ProfileController.java
+++ src/main/java/com/anonymous/usports/domain/profile/controller/ProfileController.java
@@ -1,8 +1,9 @@
 package com.anonymous.usports.domain.profile.controller;
 
-import com.anonymous.usports.domain.profile.dto.ProfileRecords;
-import com.anonymous.usports.domain.profile.dto.ProfileRecruits;
+import com.anonymous.usports.domain.profile.dto.ProfileMember;
 import com.anonymous.usports.domain.profile.service.ProfileService;
+import com.anonymous.usports.domain.record.dto.RecordListDto;
+import com.anonymous.usports.domain.recruit.dto.RecruitListDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -21,25 +22,35 @@ public class ProfileController {
 
   private final ProfileService profileService;
 
+  @ApiOperation("프로필 - 회원 정보")
+  @GetMapping("/profile/{accountName}")
+  public ResponseEntity<ProfileMember> profileRecruits(
+      @PathVariable String accountName) {
+
+    ProfileMember profileMember = profileService.profileMember(accountName);
+
+    return ResponseEntity.ok(profileMember);
+  }
+
   @ApiOperation("프로필 - 기록 글")
-  @GetMapping("/{accountName}/records")
-  public ResponseEntity<ProfileRecords> profileRecords(@PathVariable String accountName,
+  @GetMapping("/profile/{accountName}/records")
+  public ResponseEntity<RecordListDto> profileRecords(@PathVariable String accountName,
       @RequestParam(value = "page", defaultValue = "1") Integer page) {
 
-    ProfileRecords profileRecords = profileService.profileRecords(accountName, page);
+    RecordListDto recordList = profileService.profileRecords(accountName, page);
 
-    return ResponseEntity.ok(profileRecords);
+    return ResponseEntity.ok(recordList);
   }
 
 
   @ApiOperation("프로필 - 모집 글")
-  @GetMapping("/{accountName}/recruits")
-  public ResponseEntity<ProfileRecruits> profileRecruits(@PathVariable String accountName,
+  @GetMapping("/profile/{accountName}/recruits")
+  public ResponseEntity<RecruitListDto> profileRecruits(@PathVariable String accountName,
       @RequestParam(value = "page", defaultValue = "1") Integer page) {
 
-    ProfileRecruits profileRecruits = profileService.profileRecruits(accountName, page);
+    RecruitListDto recruitList = profileService.profileRecruits(accountName, page);
 
-    return ResponseEntity.ok(profileRecruits);
+    return ResponseEntity.ok(recruitList);
   }
 
 }
