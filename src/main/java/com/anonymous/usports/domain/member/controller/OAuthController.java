ed068768bb03f4e03f3a7e82dfe005652fce9c3c
jejoonlee <jejoonlee1996@gmail.com>
2023-12-31 22:37:44 +0900

    Refactor : myPageService.getInterestedSportsList 사용 대신, 매서드 만들기, 이미 member entityë 객체ê를 찾ë는 매서드가 있어서, 두번 찾을 필요성이 없다고 판단

--- src/main/java/com/anonymous/usports/domain/member/controller/OAuthController.java
+++ src/main/java/com/anonymous/usports/domain/member/controller/OAuthController.java
@@ -1,17 +1,17 @@
 package com.anonymous.usports.domain.member.controller;
 
+import com.anonymous.usports.domain.member.dto.MemberLogin;
 import com.anonymous.usports.domain.member.service.impl.CustomOAuth2MemberService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = "OAuth2")
-@Controller
-@RequestMapping("/login")
+@RestController
+@RequestMapping("/oauth2/login")
 @RequiredArgsConstructor
 @Slf4j
 public class OAuthController {
@@ -21,10 +21,20 @@ public class OAuthController {
     /**
      * 테스트 용
      */
-    @GetMapping("")
-    public String login(Model model){
-
-        log.info("hello");
-        return "login";
+    @GetMapping("/success")
+    public MemberLogin.Response loginSuccess(){
+        return MemberLogin.Response.builder()
+            .tokenDto()
+            .memberResponse()
+            .build();
     }
+//    @GetMapping("/code/{registrationId}")
+//    public String login(
+//        @RequestParam String code,
+//        @RequestParam String state,
+//        @PathVariable String registrationId
+//    ){
+//
+//        return code;
+//    }
 }
--- src/main/java/com/anonymous/usports/domain/member/security/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/anonymous/usports/domain/member/security/handler/OAuth2SuccessHandler.java
@@ -3,16 +3,15 @@ package com.anonymous.usports.domain.member.security.handler;
 import com.anonymous.usports.domain.member.dto.MemberDto;
 import com.anonymous.usports.domain.member.dto.TokenDto;
 import com.anonymous.usports.domain.member.security.TokenProvider;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
@@ -25,12 +24,11 @@ public class OAuth2SuccessHandler  extends SimpleUrlAuthenticationSuccessHandler
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         MemberDto memberDto = (MemberDto) authentication.getPrincipal();
 
-        log.info("{}", memberDto.getAttributes());
-        log.info("OAuth2 인증 성공");
-
         TokenDto token = tokenProvider.saveTokenInRedis(memberDto.getEmail());
 
-        log.info("{}", token.getAccessToken());
-        log.info("{}", token.getRefreshToken());
+        log.info("OAuth2 인증 성공");
+
+        getRedirectStrategy().sendRedirect(request, response, "http://localhost:8080/oauth2/login/success");
+
     }
 }
