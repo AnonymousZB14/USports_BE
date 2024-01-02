17d78e82a0bc5eaa612f0a3c96ce3cd6be1a20fd
jejoonlee <jejoonlee1996@gmail.com>
2024-01-02 16:28:42 +0900

    fix : change URL for kakao login

--- src/main/java/com/anonymous/usports/global/constant/UrlConstant.java
+++ src/main/java/com/anonymous/usports/global/constant/UrlConstant.java
@@ -2,7 +2,7 @@ package com.anonymous.usports.global.constant;
 
 public class UrlConstant {
 
-  public static final String USPORTS_URL = "3.39.34.245:8080";
+  public static final String USPORTS_URL = "http://3.39.34.245:8080";
 
   public static final String LOCAL_URL = "http://localhost:8080";
 
